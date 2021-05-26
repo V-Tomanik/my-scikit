80bfc9383ea3d98c4fdfe369a9215608871b5309
vv <victor.o.tomanik@gmail.com>
2021-05-25 23:39:48 -0300

    adicionando matrix_distance & testes

--- src/test/utils/test_utils_vector.py
+++ src/test/utils/test_utils_vector.py
@@ -4,13 +4,26 @@ import numpy as np
 
 class TestDistance_vector:
     
-    def test_distance_two_dimensions(self):
+    def test_distance_two_dimensions_euclidian(self):
         x = np.array([1,1])
         y = np.array([2,2])
-        assert round(uv.distance_vector(x,y),2) == 1.41
+        assert round(uv.euclidian_distance(x,y),2) == 1.41
     
-    def test_distance_tree_dimensions(self):
+    def test_distance_tree_dimensions_euclidian(self):
         x = np.array([1,1,1])
         y = np.array([2,2,2])
-        assert round(uv.distance_vector(x,y),2) == 1.73
+        assert round(uv.euclidian_distance(x,y),2) == 1.73
     
+    def test_distance_two_dimensions_minkowski(self):
+        x = np.array([1,1])
+        y = np.array([2,2])
+        assert np.round(uv.minkowski_distance(x,y,p=2),2) == 1.41
+        assert np.round(uv.minkowski_distance(x,y,p=1),2) == 2
+    
+    def test_distance_tree_dimensions_minkowski(self):
+        x = np.array([1,1,1])
+        y = np.array([2,2,2])
+        assert np.round(uv.minkowski_distance(x,y),2) == 1.73
+        assert np.round(uv.minkowski_distance(x,y,p=1),2) == 3
+
+
--- src/utils/utils_vector.py
+++ src/utils/utils_vector.py
@@ -1,22 +1,42 @@
 import numpy as np
 
 
-def distance_vector(x,y):
+def euclidian_distance(x,y):
     """
+    Exemplo
+    distance = ((p1x - p2x)² + (p1y - p2y)²)^(0.5)
     x (np.array): ponto x
     y (np.array): ponto y
     """
-    xx= np.dot(x,x)
+    xx = np.dot(x,x)
     yy= np.dot(y,y)
     distance = np.sqrt(xx - 2*np.dot(x,y) + yy)
     return distance
 
+def minkowski_distance(x,y,p=2):
+    """
+    Generalização da euclidian e manhattan distance
 
-
-
+    Para precisão usar p=2
+    """
+    if p == 1:
+        return np.sum(np.abs(y-x),axis=-1)
+    if p == 2:
+        return np.sum(np.abs(y-x)**p, axis=-1)**(1./p)
+        
+def distance_matrix(x,y):
+    x = np.asarray(x)
+    k = x.shape[1]
+    y = np.asarray(y)
+    kk = y.shape [1]
+    
+    if k != kk:
+        raise ValueError("As duas matrizes nao possuem as mesmas dimensaoes")
+    return minkowski_distance(x[:,np.newaxis,:],y[np.newaxis,:,:])
 
 if __name__ == '__main__':
+    #x = np.matrix([[1,1],[2,2],[3,3]])
+    #print(distance_matrix(x,x))
     x = np.array([1,1])
     y = np.array([2,2])
-    print(distance_vector(x,y))
-    
+    print(np.round(minkowski_distance(x,y,p=1),2))
